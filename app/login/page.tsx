fafaa598e33e0447f9581826265183895047d24e
shvmroz <shvmroz@gmail.com>
2025-08-25 17:41:20 +0500

    login screen fixes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -40,10 +40,10 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-indigo-50 dark:from-gray-900 dark:to-gray-800">
       <div className="min-h-screen grid grid-cols-1 lg:grid-cols-12">
         {/* Left Column - Slogan */}
-        <div className="lg:col-span-6 flex items-center justify-center p-8 lg:p-16">
+        <div className="hidden lg:flex lg:col-span-6 items-center justify-center p-8 lg:p-16">
           <div className="max-w-lg text-center lg:text-left">
             <div className="flex justify-center lg:justify-start mb-8">
               <div className="w-20 h-20 bg-gradient-to-r from-[#0077ED] to-[#4A9AFF] rounded-3xl flex items-center justify-center shadow-2xl">
@@ -92,7 +92,7 @@ export default function LoginPage() {
         </div>
 
         {/* Right Column - Login Form */}
-        <div className="lg:col-span-6 flex items-center justify-center p-8">
+        <div className="col-span-12 lg:col-span-6 flex items-center justify-center p-8">
           <div className="max-w-md w-full space-y-8">
             <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-xl p-8 border border-gray-100 dark:border-gray-700">
               {/* Header */}
