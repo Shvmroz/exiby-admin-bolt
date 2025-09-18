581c1e5d4db2d669b676a97068f07a0436257c1f
shvmroz <shvmroz@gmail.com>
2025-09-18 09:53:07 +0500

    Calendar Picker icon and darkmode fixed

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 
@@ -11,9 +13,13 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           'w-full h-10 px-3 rounded-md border text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 ' +
-          'bg-white text-gray-900 placeholder-gray-400 border-gray-300 ' + // light mode
-          'dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:border-gray-600 ' + // dark mode
-          className
+            'bg-white text-gray-900 placeholder-gray-400 border-gray-300 ' + // light mode
+            'dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:border-gray-600 ' + // dark mode
+            // Calendar icon styles
+            '[&::-webkit-calendar-picker-indicator]:cursor-pointer ' + // pointer cursor
+            '[&::-webkit-calendar-picker-indicator]:invert-0 ' + // light mode normal
+            'dark:[&::-webkit-calendar-picker-indicator]:invert ' + // dark mode inverted
+            className
         )}
         ref={ref}
         {...props}
